3ff3d49850fc22e6f5c2689a788d06e283c7ec7a
hyh050 <yhhan050@gmail.com>
2024-11-14 11:49:16 +0900

    [Feat] 헤드 풋터

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Header(){
+    return(
+        <div>
+            <h1>짧은 산책</h1>
+            <ul>
+                <li><a href="">채팅</a></li>
+                <li><a href="">로그인</a></li>
+            </ul>
+            <div>
+                <button>My Page<img src="" alt="" /></button>
+                
+            </div>
+        </div>
+    )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Post(){
+    return(
+        <Header></Header>
+    );
+}
