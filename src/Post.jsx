e5456912b1a080cb6e3d28e994ef319390d9fe66
hyh050 <yhhan050@gmail.com>
2024-11-14 13:44:03 +0900

    component폴더생성

--- src/Post.jsx
+++
@@ -1,8 +0,0 @@
-import Header from "./Header";
-import Footer from "./Footer";
-
-export default function Post(){
-    return(
-        <Header></Header>
-    );
-}
---
+++ src/component/posts/Post.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
+export default function Post(){
+    return(
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+        
+    );
+}
