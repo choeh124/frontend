0f43d6768d5023993eaa9d4c53211ed51d79b094
dayounglee <verdantjuly@kakao.com>
2024-11-16 14:58:30 +0900

    [Feat] Router 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Post from './component/posts/Post.jsx';
+import "./App.css";
+import Post from "./component/posts/Post.jsx";
+import Login from "./component/users/Login.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Post/>
+      <Router>
+        <Routes>
+          <Route path="/posts" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 
 export default App;
