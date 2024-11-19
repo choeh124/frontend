85e0dcde9e91d4ba33623cc694c8389cb5b3e32f
choeh124 <delicoffee@daum.net>
2024-11-19 11:58:13 +0900

    [Feat] EmailLogin

---
+++ src/component/users/EmailLogin.jsx
@@ -0,0 +1,11 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import "./EmailLogin.css";
+
+export default function Login() {
+  return <div>
+    <Header/>
+          
+    <Footer/>
+    </div>;
+}
--- src/component/users/SignUp.jsx
+++ src/component/users/SignUp.jsx
@@ -3,22 +3,16 @@ import Header from "../Header";
 
 export default function SignUp() {
   return (
-    <div>
+    <div className="login-container">
       <Header />
       <h1>회원가입</h1>
-      <br />
       E-mail<input type="text"></input>
       <button>전송</button>
-      <br />
       인증번호<input type="text"></input>
       <button>인증</button>
-      <br />
       닉네임<input type="text"></input>
-      <br />
       비밀번호<input type="text"></input>
-      <br />
       비밀번호 확인<input type="text"></input>
-      <br />
       <button>이메일 회원가입</button>
       <Footer />
     </div>
