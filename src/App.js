2b70ee49918d5600a5f12f07f83ad2e6f0ac7f2f
dayounglee <verdantjuly@kakao.com>
2024-11-22 12:33:04 +0900

    [Feat] singout route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AdminPage from "./component/adminPage/AdminPage.jsx";
 import GroupPage from "./component/groupPage/GroupPage.jsx";
 import MusicPage from "./component/musicPage/MusicPage.jsx";
 import UserUpdate from "./component/mypage/UserUpdate.jsx";
+import SignOut from "./component/users/SignOut.jsx";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
           <Route path="/group" element={<GroupPage />} />
           <Route path="/music" element={<MusicPage />} />
           <Route path="/user/edit" element={<UserUpdate />} />
+          <Route path="/user/signout" element={<SignOut />} />
         </Routes>
       </Router>
     </div>
