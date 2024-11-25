c4333cc082163b09ac7c59acb073e1e76a97d53a
hyh050 <yhhan050@gmail.com>
2024-11-25 10:45:33 +0900

    [FEAT] write button

--- src/component/posts/PostsCradList.jsx
+++ src/component/posts/PostsCradList.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import PostsCard from "./PostsCard";
 import Pagination from "./Pagination";
+import './PostsCardList.css'
+
 export default function PostsCardList() {
   const [posts, Setposts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,6 +28,10 @@ export default function PostsCardList() {
 
   return (
     <div className="PostsCardList">
+      <button onClick={()=>{
+        window.location.href = `post/write`
+      }}
+      >글쓰기</button>
       <PostsCard posts={posts}></PostsCard>
       <Pagination
         currentPage={currentPage}
