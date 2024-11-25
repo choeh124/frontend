6d5f3ce2dd32d554346f7229282a582d71d89d62
hyh050 <yhhan050@gmail.com>
2024-11-25 10:51:35 +0900

    [FIX] post cancel button color

--- src/component/post/PostCommentContent.jsx
+++ src/component/post/PostCommentContent.jsx
@@ -11,8 +11,8 @@ export default function PostCommentContent({ data }) {
         }}
       >
         <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21.75 7.25L7.25 21.75M7.25 7.25L21.75 21.75" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
+        <path d="M21.75 7.25L7.25 21.75M7.25 7.25L21.75 21.75" stroke="#F5F5F5" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
       </button>
       <WriteComment></WriteComment>
       <CommentList></CommentList>
