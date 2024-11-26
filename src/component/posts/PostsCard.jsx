050ac556cf82cedf6d6307c2a564b639ad76ab43
hyh050 <yhhan050@gmail.com>
2024-11-26 10:37:30 +0900

    [FEAT] singo button

--- src/component/posts/PostsCard.jsx
+++ src/component/posts/PostsCard.jsx
@@ -8,22 +8,23 @@ export default function PostsCard({ posts }) {
       {posts.map((post) => {
         const url = post.thumbnail ? endpoint + post.thumbnail : null; // 이미지 URL 생성
         return (
-          <div
+          <div className="PCcontent"
             key={post.id}
             onClick={(e) => {
               e.preventDefault();
               window.location.href = `/posts/${post.id}`;
             }}
           >
-            {url ? (
-              <img src={url} alt={url} />
-            ) : (
-              <div className="no-image"></div>
-            )}
-            <h3>{post.title}</h3>
-            <p>{post.nickname}</p>
-            <p>{post.createdAt.split("T")[0]}</p>
-            <p>{post.viewCount}</p>
+              {url ? (
+                <img src={url} alt={url} />
+              ) : (
+                <div className="no-image"></div>
+              )}
+                  <h3>{post.title}</h3>
+                  <p>{post.nickname}</p>
+                  <p>{post.createdAt.split("T")[0]}</p>
+                  <p>{post.viewCount}</p>
+            
           </div>
         );
       })}
