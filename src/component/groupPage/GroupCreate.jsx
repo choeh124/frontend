58fea7c6a3067216f87b96f54b63e5bfaefe882f
choeh124 <delicoffee@daum.net>
2024-11-26 11:44:58 +0900

    [Feat]group폴더내에 빈 파일들 생성

---
+++ src/component/groupPage/GroupCreate.jsx
@@ -0,0 +1,75 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import "./Group.css";
+import React, { useState } from "react";
+
+export default function GroupCreate() {
+    const [groupName, setGroupName] = useState("");
+    const [description, setDescription] = useState("");
+    const token = window.sessionStorage.getItem("Authorization");
+
+    const handleInputChange = (e) => {
+        if ("groupName" == e.target.id) {
+            setGroupName(e.target.value.trim());
+        }
+        if ("description" == e.target.id) {
+            setDescription(e.target.value.trim());
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // 기본 폼 제출 동작 방지
+
+        fetch("http://localhost:8000/api/groups", {
+            method: "POST", // POST 요청
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: JSON.stringify({ groupName, description }),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    alert("그룹생성 완료되었습니다.");
+                } else {
+                    alert("그룹생성 실패하였습니다.");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                //window.location.href = "/posts";
+            })
+            .catch((error) => { });
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="form-container">
+                    <h1>그룹 생성</h1>
+                    <div className="form-row">
+                        <label>그룹 이름</label>
+                        <input
+                            type="text"
+                            id="groupName"
+                            className="input2"
+                            onChange={handleInputChange}
+                        ></input>
+                    </div>
+                    <div className="form-row">
+                        <label>그룹 설명</label>
+                        <input
+                            type="text"
+                            id="description"
+                            className="input2"
+                            onChange={handleInputChange}
+                        ></input>
+                    </div>
+                    <button onClick={handleSubmit}>그룹 생성하기</button>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
