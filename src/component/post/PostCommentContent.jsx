34b12948d7a889538751be4ea5c330dd8075b701
dayounglee <verdantjuly@kakao.com>
2024-11-26 16:53:19 +0900

    [Feat] post comment content css

--- src/component/post/PostCommentContent.jsx
+++ src/component/post/PostCommentContent.jsx
@@ -99,29 +99,6 @@ export default function PostCommentContent() {
 
   return (
     <div className="PostCommentContent">
-      {/* ====뒤로가기버튼==== */}
-      <button
-        onClick={() => {
-          window.location.href = `/posts`;
-        }}
-      >
-        <svg
-          width="29"
-          height="29"
-          viewBox="0 0 29 29"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M21.75 7.25L7.25 21.75M7.25 7.25L21.75 21.75"
-            stroke="#F5F5F5"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
-
       {/* ====댓글쓰기==== */}
       <div className="WriteContent">
         <form onSubmit={handleSubmin}>
